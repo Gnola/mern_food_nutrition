c331c211dfcdfdf5605578fad9233bebf19bb510
G <53386620+Gnola@users.noreply.github.com>
2020-03-18 16:14:44 -0700

    added checkbox and some more styling

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       sugarSearch: 0,
       searched: [],
       searchedFoods: [],
+      checked: false
     }
   }
 
@@ -65,10 +66,18 @@ class App extends Component {
       searched: [],
       searchedFoods: []
     })
-    // loop through foods and find matches
-    for (var i = 0; i < this.state.foods.length; i++) {
-      if (this.state.foods[i].nutrients[1].value >= this.state.proteinSearch && this.state.foods[i].nutrients[2].value >= this.state.fatSearch && this.state.foods[i].nutrients[3].value >= this.state.carbSearch && this.state.foods[i].nutrients[4].value >= this.state.sugarSearch) {
-        this.state.searched.push(this.state.foods[i])
+    // loop through foods and find matches depending on checkbox
+    if (!this.state.checked) {
+      for (var i = 0; i < this.state.foods.length; i++) {
+        if (this.state.foods[i].nutrients[1].value >= this.state.proteinSearch && this.state.foods[i].nutrients[2].value >= this.state.fatSearch && this.state.foods[i].nutrients[3].value >= this.state.carbSearch && this.state.foods[i].nutrients[4].value >= this.state.sugarSearch) {
+          this.state.searched.push(this.state.foods[i])
+        }
+      }
+    } else {
+      for (var i = 0; i < this.state.foods.length; i++) {
+        if (this.state.foods[i].nutrients[1].value <= this.state.proteinSearch && this.state.foods[i].nutrients[2].value <= this.state.fatSearch && this.state.foods[i].nutrients[3].value <= this.state.carbSearch && this.state.foods[i].nutrients[4].value <= this.state.sugarSearch) {
+          this.state.searched.push(this.state.foods[i])
+        }
       }
     }
     // alert if no results are found
@@ -90,34 +99,6 @@ class App extends Component {
 
   // RESET //
   reset = () => {
-    // fetch(`/foods`)
-    // .then(res => res.json())
-    // .then((res) => {
-    //   this.setState({ // reset state completely
-    //     foods:res[0].report.foods,
-    //     searchedFoods: [],
-    //     proteinSearch: 0,
-    //     fatSearch: 0,
-    //     carbSearch: 0,
-    //     sugarSearch: 0,
-    //   }, () => {
-    //     for (var i = 0; i < this.state.foods.length; i++) {
-    //       if (this.state.foods[i].nutrients[1].value === '--') {
-    //         this.state.foods[i].nutrients[1].value = 0
-    //       }
-    //       if (this.state.foods[i].nutrients[2].value === '--') {
-    //         this.state.foods[i].nutrients[2].value = 0
-    //       }
-    //       if (this.state.foods[i].nutrients[3].value === '--') {
-    //         this.state.foods[i].nutrients[3].value = 0
-    //       }
-    //       if (this.state.foods[i].nutrients[4].value === '--') {
-    //         this.state.foods[i].nutrients[4].value = 0
-    //       }
-    //     }
-    //   })
-    // })
-    // .catch(err => console.log(err))
     this.setState({
       searched: [],
       searchedFoods: [],
@@ -129,6 +110,11 @@ class App extends Component {
     console.log(this.state);
   }
 
+  check = () => {
+    this.setState({
+      checked:!this.state.checked
+    })
+  }
 
 
   render() {
@@ -139,23 +125,25 @@ class App extends Component {
         </div>
         <div id='search-form'>
           <h1>Search</h1>
-          <p id='search-text'>Search for foods with a combination of the following nutrients by amounts (in grams)</p>
+          <p >Search for foods with a combination of the following nutrients by amounts (in grams).</p>
+          <p id='search-text'>Check the box below to find foods with LESS THAN the amounts searched for.</p>
           <form onSubmit={this.handleSubmit}>
             <div id='inputs'>
               <span className='label'>Protein: <input id='proteinSearch' value={this.state.proteinSearch} onChange={this.handleChange} /></span>
               <span className='label'>Fats: <input id='fatSearch' value={this.state.fatSearch} onChange={this.handleChange} /></span>
               <span className='label'>Carbs: <input id='carbSearch' value={this.state.carbSearch} onChange={this.handleChange} /></span>
               <span className='label'>Sugar: <input id='sugarSearch' value={this.state.sugarSearch} onChange={this.handleChange} /></span>
+              <input id='check-box' type='checkbox' value={this.state.checked} onChange={this.check}/>
             </div>
 
           <div className='btns'>
             <button id='search' value='submit'>Search</button>
-            <a id='reset' onClick={() => {this.reset()}}>Reset</a>
+            <a href='#' id='reset' onClick={() => {this.reset()}}>Reset</a>
           </div>
           </form>
         </div>
         <div id='all-food'>
-          <h2 style={(this.state.searchedFoods.length === 0) ? {display:'none'} : {display: 'block'}}>Results: {this.state.searchedFoods.length}</h2>
+          <h2 style={(this.state.searchedFoods.length === 0) ? {display:'none'} : {display: 'block'}}>Found Results: {this.state.searchedFoods.length}</h2>
           {this.state.searchedFoods.map((food, id) => (
             <Food key={id} foods={food} id={id}
               proteinSearch={this.state.proteinSearch}
@@ -165,9 +153,60 @@ class App extends Component {
             />
           ))}
         </div>
+        <footer>
+          Designed by Gianni Nola
+        </footer>
       </div>
     );
   }
 }
 
 export default App;
+
+// OG Search loop
+// for (var i = 0; i < this.state.foods.length; i++) {
+//   if (this.state.foods[i].nutrients[1].value >= this.state.proteinSearch && this.state.foods[i].nutrients[2].value >= this.state.fatSearch && this.state.foods[i].nutrients[3].value >= this.state.carbSearch && this.state.foods[i].nutrients[4].value >= this.state.sugarSearch) {
+//     this.state.searched.push(this.state.foods[i])
+//   }
+// }
+
+// OG Reset function
+// reset = () => {
+//   fetch(`/foods`)
+//   .then(res => res.json())
+//   .then((res) => {
+//     this.setState({ // reset state completely
+//       foods:res[0].report.foods,
+//       searchedFoods: [],
+//       proteinSearch: 0,
+//       fatSearch: 0,
+//       carbSearch: 0,
+//       sugarSearch: 0,
+//     }, () => {
+//       for (var i = 0; i < this.state.foods.length; i++) {
+//         if (this.state.foods[i].nutrients[1].value === '--') {
+//           this.state.foods[i].nutrients[1].value = 0
+//         }
+//         if (this.state.foods[i].nutrients[2].value === '--') {
+//           this.state.foods[i].nutrients[2].value = 0
+//         }
+//         if (this.state.foods[i].nutrients[3].value === '--') {
+//           this.state.foods[i].nutrients[3].value = 0
+//         }
+//         if (this.state.foods[i].nutrients[4].value === '--') {
+//           this.state.foods[i].nutrients[4].value = 0
+//         }
+//       }
+//     })
+//   })
+//   .catch(err => console.log(err))
+//   this.setState({
+//     searched: [],
+//     searchedFoods: [],
+//     proteinSearch: 0,
+//     fatSearch: 0,
+//     carbSearch: 0,
+//     sugarSearch: 0,
+//   })
+//   console.log(this.state);
+// }
